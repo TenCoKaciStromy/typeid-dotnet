3c7f9b9d895f7a6bd7d98c554eee87c0b2b3e229
Jakub (TcKs) Muller <muller.jakub@gmail.com>
2023-07-09 00:23:04 +0200

    progress with cli

--- src/dotnet-typeid-cli/Program.cs
+++ src/dotnet-typeid-cli/Program.cs
@@ -4,7 +4,7 @@ using TcKs.TypeId;
 using static System.Console;
 
 if (args.Length < 1)
-  return PrintHelpForNoArgs();
+  return PrintHelp();
 
 return args[0] switch {
   "new" => PrintNew(args),
@@ -18,9 +18,25 @@ _ => PrintHelpForNotSupportedCommand(args)
 };
 
 static int PrintNew(string[] args)
-  => args.Length != 2
-    ? PrintHelpForInvalidCommandParametersCount(args)
-    : PrintError(TypeId.NewId(args[1]));
+  => args.Length switch {
+    1 => PrintOk(TypeId.NewId(string.Empty)),
+    2 => args[1] switch {
+      "-h" or "--help" => PrintNewHelp(),
+      _ => PrintOk(TypeId.NewId(args[1]))
+    },
+    _ => PrintHelpForInvalidCommandParametersCount(args)
+  };
+
+static int PrintNewHelp()
+  => PrintOkLines(
+    "Generate a new TypeID using the given type prefix",
+    "",
+    "Usage:",
+    "typeid new [<type_prefix>] [flags]",
+    "",
+    "Flags:",
+    "-h, --help   help for new"
+  );
 
 static int PrintDecode(string[] args)
   => args.Length != 2
@@ -36,8 +52,7 @@ static int PrintEncode(string[] args)
       ? PrintOk(new TypeId(args[1], uuid))
       : PrintError("Encode failed. Can not parse input uuid.");
 
-static int PrintHelpForNoArgs()
-  => PrintError("No arguments was passed.");
+// >>> Shared print-help methods >>>
 
 static int PrintHelpForInvalidCommandParametersCount(string[] args)
   => PrintError($"Command ({args[0]}) has invalid count of parameters.");
@@ -46,10 +61,41 @@ static int PrintHelpForNotSupportedCommand(string[] args)
   => PrintError($"Command ({args[0]}) is not supported.");
 
 static int PrintHelp()
-  => PrintOk("TODO: print help");
+  => PrintOkLines(
+    "Type-safe, K-sortable, globally unique identifiers",
+    "",
+    "Usage:",
+    "  typeid [flags]",
+    "  typeid [command]",
+    "",
+    "Available Commands:",
+    "  completion  Generate the autocompletion script for the specified shell",
+    "  decode      Decode the given TypeID into a UUID",
+    "  encode      Encode the given UUID into a TypeID using the given type prefix",
+    "  help        Help about any command",
+    "  new         Generate a new TypeID using the given type prefix",
+    "",
+    "Flags:",
+    "  -h, --help   help for typeid",
+    "",
+    "  Use \"typeid [command] --help\" for more information about a command."
+  );
+
+// >>> Generic helper methods >>>
 
 static int PrintOk(object valueToPrint)
   => Print(0, valueToPrint);
+
+static int PrintOkLines(params object[] lines) {
+  if (lines is null)
+    return 0;
+
+  foreach (var line in lines) {
+    WriteLine(line);
+  }
+
+  return 0;
+}
 
 static int PrintError(object errorToPrint)
   => Print(-1, errorToPrint);
